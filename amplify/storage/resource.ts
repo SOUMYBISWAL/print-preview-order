1c7f701415cf1c298f8dc7e46b0a9b5a2d1e72db
SOUMYABISWAL <soumyaranjan@4387gmail.com>
2025-08-22 23:39:36 +0530

    change storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,7 +1,7 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'amplifyTeamDrive',
+  name: 'userfiles', // Add a unique name for your storage resource
   access: [
     {
       allow: 'authenticated',
