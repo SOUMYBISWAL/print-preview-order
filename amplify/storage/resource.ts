8452be0750f35bde55c9133d4797eaf469c86a79
SOUMYABISWAL <soumyaranjan@4387gmail.com>
2025-08-22 23:15:23 +0530

    backend

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,14 +4,14 @@ export const storage = defineStorage({
   name: 'printuserfile',
   access: [
     {
-      // Allow authenticated users full access
       allow: 'authenticated',
       operations: ['read', 'write', 'delete', 'list'],
+      level: 'protected'
     },
     {
-      // Allow unauthenticated users to upload and list files
       allow: 'guest',
       operations: ['read', 'write', 'list'],
+      level: 'public'
     }
   ]
 });
