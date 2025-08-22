773f0c5433c789729c81662ab411091137a2c95d
SOUMYABISWAL <soumyaranjan@4387gmail.com>
2025-08-22 21:37:49 +0530

    change storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,17 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'printuserfile'
+  name: 'printuserfile',
+  access: [
+    {
+      // Allow authenticated users full access
+      allow: 'authenticated',
+      operations: ['read', 'write', 'delete', 'list'],
+    },
+    {
+      // Allow unauthenticated users to upload and list files
+      allow: 'guest',
+      operations: ['read', 'write', 'list'],
+    }
+  ]
 });
