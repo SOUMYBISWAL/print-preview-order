0a7fa52c6507e814c1eaab60716246bc46fbdff4
SOUMYABISWAL <soumyaranjan@4387gmail.com>
2025-08-23 19:12:39 +0530

    dskhfjk

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
+import { Amplify } from 'aws-amplify';
+import amplifyOutputs from '../amplify_outputs.json';
 
 const queryClient = new QueryClient()
 
 console.log('PrintLite - Frontend-only version loaded');
+
+Amplify.configure(amplifyOutputs);
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
