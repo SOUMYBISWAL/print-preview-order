2277fe9857bda94fdc8082358b8493ffe92a49f6
SOUMYABISWAL <soumyaranjan@4387gmail.com>
2025-08-23 09:29:41 +0530

    change

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,17 +1,9 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'userfiles', // Use only lowercase, numbers, and hyphens
-  access: [
-    {
-      allow: 'authenticated',
-      operations: ['read', 'write', 'delete', 'list'],
-      level: 'protected'
-    },
-    {
-      allow: 'guest',
-      operations: ['read', 'write', 'list'],
-      level: 'public'
-    }
+  name: 'userfiles',
+  access: (grant) => [
+    grant('authenticated').to(['read', 'write', 'delete', 'list']).level('protected'),
+    grant('guest').to(['read', 'write', 'list']).level('public')
   ]
 });
